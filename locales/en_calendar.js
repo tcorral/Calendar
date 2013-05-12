5740c3f318a90214c2f9bc55f8508fce81527cfe
tcorral <amischol@gmail.com>
2013-05-12 22:01:37 +0200

    First Commit

---
+++ locales/en_calendar.js
@@ -0,0 +1,55 @@
+var EN_calendar = {
+  firstDayOfWeek: 0,
+  prev: 'Previous month',
+  next: 'Next month',
+  weekdays: {
+    long: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ],
+    short: [
+      "Sun",
+      "Mon",
+      "Tue",
+      "Wed",
+      "Thu",
+      "Fri",
+      "Sat"
+    ]
+  },
+  months: {
+    long: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ],
+    short: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ]
+  }
+};
---
+++ locales/es_calendar.js
@@ -0,0 +1,55 @@
+var ES_calendar = {
+  firstDayOfWeek: 1,
+  prev: 'Mes anterior',
+  next: 'Mes siguiente',
+  weekdays: {
+    long: [
+      "Domingo",
+      "Lunes",
+      "Martes",
+      "Miércoles",
+      "Jueves",
+      "Viernes",
+      "Sábado"
+    ],
+    short: [
+      "Dom",
+      "Lun",
+      "Mar",
+      "Mie",
+      "Jue",
+      "Vie",
+      "Sab"
+    ]
+  },
+  months: {
+    long: [
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre'
+    ],
+    short: [
+      'Ene',
+      'Feb',
+      'Mar',
+      'Abr',
+      'May',
+      'Jun',
+      'Jul',
+      'Ago',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dic'
+    ]
+  }
+};
